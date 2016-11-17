e7949c7cd1d789dcae3716aa81ddf9d363704a0e
Nicholas Delpino <Nicky@Nicholas-MacBook-Pro.local>
2016-11-16 21:39:49 -0500

    Create Instance of Express

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ var mongoose = require('mongoose');
 
 //Require Schemas
 var Article = require('./server/model.js');
+
+// Create Instance of Express
+var app = express();
+var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
