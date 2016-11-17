a34d087139d5a3ef8d46691a033ab0cca7a60d23
Nicholas Delpino <Nicky@Nicholas-MacBook-Pro.local>
2016-11-16 21:47:54 -0500

    create routes

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 app.use(express.static('./public'));
 
+// -------------------------------------------------
 
 // MongoDB Configuration
 mongoose.connect('mongodb://admin:reactrocks@ds023593.mlab.com:23593/heroku_pg676kmk');
@@ -32,3 +33,61 @@ db.on('error', function (err) {
 db.once('open', function () {
     console.log('Mongoose connection successful.');
 });
+
+// -------------------------------------------------
+
+// Main Route
+app.get('/', function(req, res){
+    res.sendFile('./public/index.html');
+});
+
+// Route to get all saved articles
+app.get('/api/saved', function(req, res) {
+
+    Article.find({})
+        .exec(function(err, doc){
+
+            if(err){
+                console.log(err);
+            }
+            else {
+                res.send(doc);
+            }
+        })
+});
+
+// Route to add an article to saved list
+app.post('/api/saved', function(req, res){
+    var newArticle = new Article(req.body);
+
+    console.log(req.body)
+
+    var title = req.body.title;
+    var date = req.body.date;
+    var url = req.body.url;
+
+    newArticle.save(function(err, doc){
+        if(err){
+            console.log(err);
+        } else {
+            res.send(doc._id);
+        }
+    });
+});
+
+// Route to delete an article from saved list
+app.delete('/api/saved/', function(req, res){
+
+    var url = req.param('url');
+
+    Article.find({"url": url}).remove().exec(function(err, data){
+        if(err){
+            console.log(err);
+        }
+        else {
+            res.send("Deleted");
+        }
+    });
+});
+
+// -------------------------------------------------
