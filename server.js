308dd8ca71b8bfa8f4b90ae332cae916dd17cf97
Nicholas Delpino <Nicky@Nicholas-MacBook-Pro.local>
2016-11-16 21:48:52 -0500

    listening on port?

--- server.js
+++ server.js
@@ -91,3 +91,7 @@ app.delete('/api/saved/', function(req, res){
 });
 
 // -------------------------------------------------
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
