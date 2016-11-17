d9bf75722b5a77b2c59cd9c1f85a08fd718433a6
Nicholas Delpino <Nicky@Nicholas-MacBook-Pro.local>
2016-11-16 21:38:14 -0500

    Require Schemas

--- server.js
+++ server.js
@@ -3,3 +3,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+
+//Require Schemas
+var Article = require('./server/model.js');
