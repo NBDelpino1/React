d3ed2e2f04f22bf5123af9bd27ff786c63aaf600
Nicholas Delpino <Nicky@Nicholas-MacBook-Pro.local>
2016-11-16 21:40:47 -0500

    Run Morgan for Logging

--- server.js
+++ server.js
@@ -10,3 +10,10 @@ var Article = require('./server/model.js');
 // Create Instance of Express
 var app = express();
 var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
+
+// Run Morgan for Logging
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
